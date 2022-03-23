fae72929490a934bb63081cfd87e0025449c9527
Johnny-Yh <1351650904@qq.com>
2022-03-23 20:51:12 +0800

    update

--- main.go
+++ main.go
@@ -5,18 +5,16 @@ import "fmt"
 func main() {
     var command string
     for {
-        fmt.Print("enter your command:")
+        fmt.Print("请输入你的指令:")
 		fmt.Scan(&command)
         switch command {
-		case "h":
-            fmt.Println("Help!")
-        case "p":
-            fmt.Println("Print something!")
-        case "q":
-			fmt.Println("Quit!")
-            return
-        default:
-            fmt.Println("Command is undefined!")
+            case "p":
+                fmt.Println("Print something!")
+            case "q":
+                fmt.Println("Quit!")
+                return
+            default:
+                fmt.Println("Command undefined!")
         }
  
     }
